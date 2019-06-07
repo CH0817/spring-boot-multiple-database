f52a3bbab5c5083d59a9d64bebab1a539f392a54
Rex Yu <yu.chenhang@gmail.com>
2019-06-08 02:02:12 +0800

    modify mybatis configurations

--- src/main/java/tw/com/rex/springbootmultipledatabase/config/MariaDbConfig.java
+++ src/main/java/tw/com/rex/springbootmultipledatabase/config/MariaDbConfig.java
@@ -1,24 +1,50 @@
 package tw.com.rex.springbootmultipledatabase.config;
 
-import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
 
 @Configuration
-@MapperScan(basePackages = {"classpath:mapper/mariaDb/*.xml"})
+@MapperScan(basePackages = {"tw.com.rex.springbootmultipledatabase.mapper.mariadb"},
+            sqlSessionTemplateRef = "mariadbSqlSessionTemplate")
 public class MariaDbConfig {
 
     @Bean(name = "mariaDbDataSource")
-    @Primary
-    @ConfigurationProperties("spring.datasource.mariadb")
+    @ConfigurationProperties("datasource.mariadb")
     public DataSource mariaDbDataSource() {
-        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "mariadbTransactionManager")
+    public DataSourceTransactionManager masterTransactionManager(
+            @Qualifier("mariaDbDataSource") DataSource mariaDbDataSource) {
+        return new DataSourceTransactionManager(mariaDbDataSource);
+    }
+
+    @Bean(name = "mariadbSqlSessionFactory")
+    public SqlSessionFactory masterSqlSessionFactory(@Qualifier("mariaDbDataSource") DataSource mariaDbDataSource)
+            throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(mariaDbDataSource);
+        sessionFactory.setMapperLocations(
+                new PathMatchingResourcePatternResolver().getResources("classpath:mapper/mariadb/*.xml"));
+        return sessionFactory.getObject();
+    }
+
+    @Bean(name = "mariadbSqlSessionTemplate")
+    public SqlSessionTemplate testSqlSessionTemplate(
+            @Qualifier("mariadbSqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
     }
 
 }
--- src/main/java/tw/com/rex/springbootmultipledatabase/config/MySqlConfig.java
+++ src/main/java/tw/com/rex/springbootmultipledatabase/config/MySqlConfig.java
@@ -1,22 +1,55 @@
 package tw.com.rex.springbootmultipledatabase.config;
 
-import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
 
 @Configuration
-@MapperScan(basePackages = {"classpath:mapper/mysql/*.xml"})
+@MapperScan(basePackages = {"tw.com.rex.springbootmultipledatabase.mapper.mysql"},
+            sqlSessionTemplateRef = "mysqlSqlSessionTemplate")
 public class MySqlConfig {
 
-    @Bean(name = "masterDataSource")
-    @ConfigurationProperties("spring.datasource.mysql")
-    public DataSource masterDataSource() {
-        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    @Bean(name = "mysqlDataSource")
+    @Primary
+    @ConfigurationProperties(prefix = "datasource.mysql")
+    public DataSource mysqlDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "mysqlTransactionManager")
+    @Primary
+    public DataSourceTransactionManager mysqlTransactionManager(
+            @Qualifier("mysqlDataSource") DataSource mysqlDataSource) {
+        return new DataSourceTransactionManager(mysqlDataSource);
+    }
+
+    @Bean(name = "mysqlSqlSessionFactory")
+    @Primary
+    public SqlSessionFactory mysqlSqlSessionFactory(@Qualifier("mysqlDataSource") DataSource mysqlDataSource)
+            throws Exception {
+        final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(mysqlDataSource);
+        sessionFactory.setMapperLocations(
+                new PathMatchingResourcePatternResolver().getResources("classpath:mapper/mysql/*.xml"));
+        return sessionFactory.getObject();
+    }
+
+    @Bean(name = "mysqlSqlSessionTemplate")
+    @Primary
+    public SqlSessionTemplate mysqlSqlSessionTemplate(
+            @Qualifier("mysqlSqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
     }
 
 }
